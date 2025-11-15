0eb91dbf83201150d3a9bf27751dc124d0757401
David Prospérin <32713756+dprosperin@users.noreply.github.com>
2025-11-15 23:05:04 +0100

    Test avec un élément structurant de taille 5x5

--- main.cpp
+++ main.cpp
@@ -2,29 +2,39 @@
 #include "chargesauve.h"
 #include  "image.h"
 #include "outils.h"
+#include <cassert>
 
 using namespace std;
 
 int main() {
     cout << "=== Binarisation de l'image  ===" << endl;
-    auto image_kodie512x512 = createImage();
+    auto image_kodie = createImage();
     bool ok = false;
 
     int seuil = 25;
 
-    loadPgm("/Users/davidprosperin/CLionProjects/smp_tp3/tp3-images/kodie512x512.pgm", image_kodie512x512, ok);
+    loadPgm("/Users/davidprosperin/CLionProjects/smp_tp3/tp3-images/kodie512x512.pgm", image_kodie, ok);
     cout << ok << endl;
 
     if (ok) {
-        seuillage(image_kodie512x512, seuil);
+        seuillage(image_kodie, seuil);
     }
 
     string sortieFichier = "/Users/davidprosperin/CLionProjects/smp_tp3/tp3-images/kodie512x512seuil"
                      + to_string(seuil)
                      + ".pgm";
 
-    savePgm(sortieFichier, image_kodie512x512);
-    delete image_kodie512x512;
+    savePgm(sortieFichier, image_kodie);
+    delete image_kodie;
+
+    cout << "=== Chargement de l'image kodie512x512seuil50 ===" << endl;
+
+    auto image_kodie_seuil50 = createImage();
+    bool ok2 = false;
+
+    loadPgm("/Users/davidprosperin/CLionProjects/smp_tp3/tp3-images/kodie512x512seuil50.pgm", image_kodie_seuil50, ok2);
+
+    assert(ok2 && "Problème lors du chargement de l'image kodie512x512seuil50.pgm");
 
     cout << "=== Dilatation ===" << endl;
 
@@ -41,21 +51,45 @@ int main() {
     element1->valeurs[1][0] = 0;
     element1->valeurs[1][2] = 0;
 
-    auto image2 = createImage();
-    bool ok2 = false;
+    auto image_contour = createImage(image_kodie_seuil50->h, image_kodie_seuil50->w);
 
-    loadPgm("/Users/davidprosperin/CLionProjects/smp_tp3/tp3-images/kodie512x512seuil20.pgm", image2, ok2);
+    dilatation(image_kodie_seuil50, image_contour, element1, 0);
 
-    auto image_contour = createImage(image2->h, image2->w);
-
-    dilatation(image2, image_contour, element1, 99);
-
-    string sortieFichierContour = "/Users/davidprosperin/CLionProjects/smp_tp3/tp3-images/kodie512x512seuil20ContourVariable.pgm";
+    string sortieFichierContour = "/Users/davidprosperin/CLionProjects/smp_tp3/tp3-images/kodie512x512seuil20ContourVariable2.pgm";
     savePgm(sortieFichierContour, image_contour);
+
+    cout << "==== Dilatation avec un élément structurant de taille 5x5 ==== " << endl;
+
+    auto element5x5 = createElement(5, 5);
+    auto image_kodie_dilatation5x5 = createImage(image_kodie_seuil50->h, image_kodie_seuil50->w);
+
+    /* Motif de l'element structurant:
+        255,   255,   0,   255,   255,
+        255,   255,   0,   255,   255,
+          0,     0,   0,     0,     0,
+        255,   255,   0,   255,   255,
+        255,   255,   0,   255,   255,
+    */
+
+    element5x5->valeurs[0][2] = 0;
+    element5x5->valeurs[1][2] = 0;
+    element5x5->valeurs[2][2] = 0;
+    element5x5->valeurs[3][2] = 0;
+    element5x5->valeurs[4][2] = 0;
+
+    element5x5->valeurs[2][0] = 0;
+    element5x5->valeurs[2][1] = 0;
+    element5x5->valeurs[2][3] = 0;
+    element5x5->valeurs[2][4] = 0;
+
+    dilatation(image_kodie, image_kodie_dilatation5x5, element5x5, 0);
+
+    savePgm("/Users/davidprosperin/CLionProjects/smp_tp3/tp3-images/kodie512x512seuil20dilatation5x5.pgm", image_kodie_dilatation5x5);
 
     delete element1;
     delete image_contour;
-    delete image2;
+    delete image_kodie_seuil50;
+    delete element5x5;
 
     return 0;
 }
